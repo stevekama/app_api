29b5e1e076d79fc73feb04cba9de58f04700d3d0
Stevekama <stevekamahertz@gmail.com>
2021-02-12 10:44:29 +0300

    run seeder

---
+++ database/seeders/ArticlesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach(range(1, 100) as $index){
+            DB::table('articles')->insert([
+                'title'=>$faker->sentence(5),
+                'body'=>$faker->paragraph(4)
+            ]);
+        }
+    }
+}
